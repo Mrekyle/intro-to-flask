464d20ffaf09e847e599842c9056168f143419f9
Kyle Chart <64297454+Mrekyle@users.noreply.github.com>
2023-07-08 12:04:12 +0100

    Add multiple pages. Add templates to avoid repeated code.

--- run.py
+++ run.py
@@ -4,11 +4,18 @@ from flask import Flask, render_template # Allows us to render an external html 
 
 app = Flask(__name__)
 
+"""
+The below lines of code are the app routing of the html pages. 
+First defining the page route name for the address bar
+Then defining the function where the page is located on the server so the page can be rendered by the flask framework
+"""
+
 # Says if the app is running on the correct file path, then display the hello world string
 @app.route("/") 
 # Function that returns the index.html file on the root level due to the app.route('/' ) method
 def index():
     return render_template("index.html") # Flask looks for the file in a folder called 'templates' at the same level as the python file
+    # return "Hello World" Renders a basic string of text on the web page
 
 @app.route("/about")
 def about():
@@ -23,6 +30,10 @@ def contact_us():
 @app.route("/careers")
 def careers():
     return render_template("careers.html")
+
+"""
+Running a basic server using the python/flask frameworks
+"""
 
 # Setting up the server for the application to run on
 if __name__ == "__main__":
